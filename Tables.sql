8d53b1617d8c683f2ff5c0a52d6394ebefdd6ccf
SahrishB <79950250+SahrishB@users.noreply.github.com>
2021-03-05 16:35:17 -0800

    Tables.sql
    Added tmpx table

--- Tables.sql
+++ Tables.sql
@@ -1,3 +1,29 @@
+CREATE TABLE tmpx
+  (
+     transaction_id SERIAL PRIMARY KEY,
+     order_id       VARCHAR (50),
+     order_date     DATE,
+     ship_date      DATE,
+     ship_mode      VARCHAR (50),
+     customer_id    VARCHAR (50),
+     customer_name  VARCHAR (50),
+     segment        VARCHAR (50),
+     country        VARCHAR (50),
+     city           VARCHAR (50),
+     state          VARCHAR (50),
+     postal_code    VARCHAR (50),
+     region         VARCHAR (50),
+     product_id     VARCHAR (50),
+     category       VARCHAR (50),
+     sub_category   VARCHAR (50),
+     product_name   VARCHAR (500),
+     sales          NUMERIC,
+     quantity       INTEGER,
+     discount       NUMERIC,
+     profit         NUMERIC
+  ); 
+
+
 CREATE TABLE product
   (
      product_id     VARCHAR (300) PRIMARY KEY,
