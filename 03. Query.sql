9076b6822331a4313fbd6f312d981dbcc997c631
SahrishB <79950250+SahrishB@users.noreply.github.com>
2021-03-09 18:36:45 -0800

    Update 03. Query.sql
    Updated query description

--- 03. Query.sql
+++ 03. Query.sql
@@ -1,4 +1,6 @@
----> Find the customers from different regions who ordered products from different categories through the first class mode with total sales of more than 5000
+-- (1) Find the customers from different regions who ordered products from different categories 
+-- through the first class mode with total sales more than 5000
+
 SELECT customer_name,
        region,
        category,
@@ -18,14 +20,15 @@ HAVING Round(Sum(sales)) > 5000
 ORDER  BY order_date; 
 
 
----> Write a query to fetch only the first name(string before space) from the customer_name column of customer table.
+-- (2) Write a query to fetch the first name (string before whitespace) from the customer_name column of customer table.
 
 SELECT DISTINCT(Split_part(customer_name, ' ', 1) ) AS first_name
 FROM   customer
 ORDER  BY first_name;
 
 
----> Write a SQL query to find the nth highest sales from transaction table. Example: finding 4th highest sales from transaction table 
+-- (3) Write a SQL query to find the nth highest sale from transactions table. 
+-- Example: Find 4th highest sale from transactions table 
 
 SELECT   transaction_id,
          sales
@@ -33,15 +36,15 @@ FROM     transactions
 ORDER BY sales DESC OFFSET 3 LIMIT 1;
 
 
----> Write a SQL query to count the different types of ship_mode sorted by desc order
+-- (4) Write a SQL query to count different types of ship_mode sorted in desc order
 
 SELECT ship_mode,
        Count(*) AS count_of_shipping
 FROM   transactions
 GROUP  BY ship_mode
-ORDER  BY count_of_shipping DESC 
+ORDER  BY count_of_shipping DESC;
 
----> OR CASE statement <----
+--- Alternate solution using CASE statement
 
 SELECT Sum(CASE ship_mode
              WHEN 'Standard Class' THEN 1
@@ -59,10 +62,10 @@ SELECT Sum(CASE ship_mode
              WHEN 'Same Day' THEN 1
              ELSE 0
            END) AS Same_Day
-FROM   transactions 
+FROM   transactions;
 
 
----> Find the names and id's of all the products that were ordered in 2019
+-- (5) Find the names and ids of all the products that were ordered in 2019
 
 SELECT product_id,
        product_name
@@ -70,20 +73,23 @@ FROM   product
 WHERE  product_id IN (SELECT transactions.product_id
                       FROM   transactions
                       WHERE  order_date BETWEEN '2019-01-01' AND '2019-12-31')
-ORDER  BY product_id 
+ORDER  BY product_id;
 
 
----> Find the order_id and sales that are greater than average sales
+-- (6) Find the order_id and sales that are greater than average sales
+
 SELECT order_id,
        Round(sales, 2) AS sales
 FROM   transactions
 WHERE  sales > (SELECT Round(Avg(sales), 2)
                 FROM   transactions)
-ORDER  BY sales DESC 
+ORDER  BY sales DESC;
 
 
----> The company needs to track customer, order and product id, customer name, city, ship date and mode of shipping in technology category along with the sub category department to which the product belongs for the year 2020, as the items were not delivered on time due to COVID 
---NOTE: the co. needs the info multiple times for few months
+-- (7) In the year 2020, various items were not delivered on time due to COVID. 
+-- The company needs to track customer id, order id, product id, customer name, customer city,sub category, 
+-- ship date and mode of shipping in category "technology".
+-- Note: The company needs to query this information multiple times without having to write complex SQL query.
 
 CREATE VIEW cust_info_2020
 AS
@@ -103,14 +109,14 @@ AS
                  ON product.product_id = transactions.product_id
   WHERE  category = 'Technology'
          AND order_date BETWEEN '2020-01-01' AND '2020-12-31'
-  ORDER  BY order_date 
+  ORDER  BY order_date; 
 
-SELECT * FROM   cust_info_2020
+SELECT * FROM cust_info_2020;
 
 
----> The company wants to gift coupons worth 70% off for premium customers, 50% off for plus customers and 10% off for others. 
--- Find the customers and categorise them. Premium customer's total spending > 10,000 and Plus customer's between 5000 and 9999. The sales made by the company are purchases made by a customer.
-
+-- (8) The company wants to gift coupons worth 70% off for premium customers, 50% off for plus customers and 10% off for others. 
+-- Find the customers and categorise them. Premium customers total spending > 10,000 and Plus customers between 5000 and 9999. 
+-- The sales made by the company are purchases made by a customer.
 
 SELECT customer_name,
        Round(Sum(sales)) AS customer_purchases,
